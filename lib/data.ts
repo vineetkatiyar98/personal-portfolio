1530d4cc06413f74ad4673ae8e87991daf7095d3
vineetkatiyar98 <104678201+vineetkatiyar98@users.noreply.github.com>
2024-01-01 09:40:51 +0530

    first commit

---
+++ lib/data.ts
@@ -0,0 +1,93 @@
+import React from "react";
+import { FaReact } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
+import vinzone from "@/public/vinzone.png";
+import movie from "@/public/movie.png";
+import blog from "@/public/blog.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "Diploma In Computer Science",
+    location: "Mohammadi Kheri, Uttar pradesh",
+    description:
+      "I completed My 3 years diploma in Computer Science and engineering from govt polytechnic Mohammadi Kheri.",
+    icon: React.createElement(LuGraduationCap),
+    date: "2022",
+  },
+  {
+    title: "Apprenticeship(Full Stack)",
+    location: "Techpile Technology pvt ltd, Lucknow",
+    description:
+      "My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+    icon: React.createElement(FaReact),
+    date: "2022-2023",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "Vinzone",
+    description:
+      "I worked as a full-stack developer on this Project.This is amazon clone",
+    tags: ["Next.js", "MongoDB", "Tailwind", "Prisma", "server-actions"],
+    imageUrl: vinzone,
+  },
+  {
+    title: "Movie",
+    description:
+      "BookMyShow App clone. Here we can book our movie sheet if sheet available in your given date and time",
+    tags: ["React", "TypeScript", "Next.js", "CSS", "nodejs"],
+    imageUrl: movie,
+  },
+  {
+    title: "Blog App",
+    description:
+      "We can read and write Latest blogs. And search with the basis of categories",
+    tags: ["React", "Next.js", "MongoDb", "CSS", "TypeScript", "nodejs"],
+    imageUrl: blog,
+  },
+] as const;
+
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Git",
+  "Tailwind",
+  "Prisma",
+  "MongoDB",
+  "Redux-ToolKit",
+  "Express",
+  "Framer Motion",
+] as const;
